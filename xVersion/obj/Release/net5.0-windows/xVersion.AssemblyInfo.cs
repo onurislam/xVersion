e296623f965fac47e472692d889f2490381901f4
Onur İslam <onurislam@outlook.com>
2024-10-22 17:42:53 +0300

    last update

--- xVersion/obj/Release/net5.0-windows/xVersion.AssemblyInfo.cs
+++ xVersion/obj/Release/net5.0-windows/xVersion.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("xVersion")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df772a03c9c3f137baaa7edef336ce362390ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("xVersion")]
